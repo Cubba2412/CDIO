e5be08b2725c6e249fff0a0fbfedf503e81ed1e7
ali <ali.jawesh5@gmail.com>
2020-09-28 14:51:10 +0200

    users input

---
+++ src/spil/Input.java
@@ -0,0 +1,17 @@
+package spil;
+
+public class Input {
+
+System.out.println("Enter player1 name:");
+    String player1Name = scanner.nextLine();
+    player1 = new Player(player1Name);
+
+        System.out.println("Enter player2 name:");
+    String player2Name = scanner.nextLine();
+    player2 = new Player(player2Name);
+
+        System.out.println();
+
+    //player1 starts the game
+    currentPlayer = player1;
+}
