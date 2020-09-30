13e0f1231fe5c404708e4b7828ed680e6fa798ed
Rivano28 <52214279+Rivano28@users.noreply.github.com>
2020-09-30 22:04:38 +0200

    Created function ThrowDice where both dice will be thrown

--- src/spil/RafleCup.java
+++ src/spil/RafleCup.java
@@ -5,7 +5,7 @@ public class RafleCup
 {
     private  Dice Dice1;
     private  Dice Dice2;
-
+    private int ValueOfDice1, ValueOfDice2;
     //Constructor, this is what the raflecup contains which is two dice
     public void rafleCup()
     {
@@ -13,6 +13,15 @@ public class RafleCup
      Dice1 = new Dice();
      //Creating second dice
      Dice2 = new Dice();
+    }
+
+    //Throws first and second dice simultaneously
+    public void throwDice()
+    {
+     //Throwing the first dice
+     ValueOfDice1 = Dice1.roll();
+     //Throwing secod dice
+     ValueOfDice2 = Dice2.roll();
     }
 }
 
